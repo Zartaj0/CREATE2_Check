5a7172ca4899a8bb034d5c865a0d2528540a3e87
zartaj <zartaj.afsar@gmail.com>
2023-08-04 22:50:23 +0530

    nonce increment test

--- contracts/CREATE2.sol
+++ contracts/CREATE2.sol
@@ -5,10 +5,20 @@ contract ToBeDeployed {
     string hello = "deployed via Create2";
 }
 
+contract Write {
+    string writeOnMe;
+
+    function wirteFun(string memory _write) external {
+        writeOnMe = _write;
+    }
+}
+
 contract Create2 {
     event Deployed(address contractAddres);
-    address public ContractDeployedOn;
+    address public CREATE2;
+    address public CREATE;
 
+    /// @notice this function calculates the address for deployment via CREATE2
     function getAddr(uint256 salt) public view returns (address) {
         bytes memory bytecode = type(ToBeDeployed).creationCode;
 
@@ -28,10 +38,22 @@ contract Create2 {
                 )
             );
     }
-
+    /// @notice this function deploys via CREATE2
     function DeployViaCreate2(uint256 salt) public {
         ToBeDeployed to = new ToBeDeployed{salt: bytes32(salt)}();
         emit Deployed(address(to));
-        ContractDeployedOn = address(to);
+        CREATE2 = address(to);
+    }
+    
+    /// @notice this function deploys via CREATE
+    function DeployViaCreate() public {
+        ToBeDeployed to = new ToBeDeployed();
+        emit Deployed(address(to));
+        CREATE = address(to);
+    }
+
+    /// @notice this function is to check if nonce increases after calling a write function
+    function write(address _contract) external {
+        Write(_contract).wirteFun("Called This Via contract");
     }
 }
